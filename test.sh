0c57cb4430e7494de18a88789a64bd31b3a07de8
Owen Buckley <owenbuckley13@gmail.com>
2022-11-11 14:04:53 -0500

    adjust ordering

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 export ENDPOINT='https://www.analogstudios.net/api/albums';
 export EXPECTED=1;
 
-# test node-fetch
-node ./src/node-fetch.js
-
-# test request
-node ./src/request.js
-
 # test native fetch
 node ./src/fetch.js
 
+# test node-fetch
+node ./src/node-fetch.js
+
 # test native http(s)
 node ./src/http.js
+
+# test request
+node ./src/request.js
